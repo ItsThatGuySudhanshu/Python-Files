1cdd70bce61cd8c5b005f41e661a340076c6388f
ItsThatGuySudhanshu <sudhanshu.moghe@gmail.com>
2019-12-26 23:02:55 +0000

    Uploaded file

---
+++ TicTacToe.py
@@ -0,0 +1,28 @@
+import pprint
+import random
+
+def printBoard(board):
+  print(board["top-L"]+ "|"+ board["top-M"]+ "|" + board["top-R"])
+  print("=====")
+  print(board["mid-L"]+ "|"+ board["mid-M"]+ "|" + board["mid-R"])
+  print("=====")
+  print(board["bot-L"]+ "|"+ board["bot-M"]+ "|" + board["bot-R"])
+
+def userXOrO():
+  
+  xOrO = input("Do you choose to be X or O? ")
+  xOrO = xOrO.lower()
+  if (xOrO == )
+def main():
+  printBoard(theboard)
+
+theboard = {"top-L": " ", 
+         "top-M": " ",
+         "top-R": " ",
+         "mid-L": " ",
+         "mid-M": " ",
+         "mid-R": " ",
+         "bot-L": " ",
+         "bot-M": " ",
+         "bot-R": " "}
+
